f1f84d2cc017794b81a9bb1a290bb349e261b519
Taylor <49215349+tdean787@users.noreply.github.com>
2021-04-16 16:01:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p> Hello {props.name} </p>
     </div>
-  );
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log('hello')
+return (  <div>
+  <Hello name="Taylor" />
+    <p>It is now {now.toString()}</p>
+    <p>
+      {a} + {b} equals {a+b}
+    </p>
+  </div>)
 }
 
 export default App;
