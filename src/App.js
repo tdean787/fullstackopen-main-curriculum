bc121bd685ee0dbcf68f64adc23685b4cde7fe57
Taylor <49215349+tdean787@users.noreply.github.com>
2021-04-18 13:39:08 -0500

    completed unicafe and anecdotes exercises

--- src/App.js
+++ src/App.js
@@ -1,166 +1,117 @@
 import React, { useState } from "react";
 
-//destructuring. take props passed in directly and assign properties
-const Hello = ({ name, age }) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear();
-    return yearNow - age;
-  };
-  return (
-    <div>
-      <p>
-        {" "}
-        Hello {name}. Apparently, you are {age} years old.{" "}
-      </p>
-      <p>Maybe you were born in {bornYear()}?</p>
-    </div>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.body} = {props.exercises}
-      </p>
-    </div>
-  );
-};
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1> {props.course}</h1>
-    </div>
-  );
-};
-
-const Content = (props) => {
-  const list = document.querySelector(".list");
-
-  return (
-    <div>
-      <ul class="list">
-        {props.parts.forEach((element) => {
-          <Part body={element.name} exercises={element.exercise} />;
-        })}
-        <Part body={props.parts[0].name} exercises={props.parts[0].exercises} />
-        <Part body={props.parts[1].name} exercises={props.parts[1].exercises} />
-        <Part body={props.parts[2].name} exercises={props.parts[2].exercises} />
-      </ul>
-    </div>
-  );
-};
-
-const Total = (props) => {
-  let sum = 0;
-  return (
-    <div>
-      <p>
-        Number of exercises
-        {props.parts.forEach((el) => {
-          sum += el.exercises;
-        })}
-        {sum}
-      </p>
-    </div>
-  );
-};
-
-const Display = ({ counter }) => <div>{counter}</div>;
-
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>The app is used by pressing buttons</div>;
-  }
-  return <div>button press history: {props.allClicks.join(" ")}</div>;
+const Statistic = (props) => {
+  return (
+    <tr>
+      <td> {props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  );
 };
 
-const App = (props) => {
-  const [counter, setCounter] = useState(0);
+const Statistics = (props) => {
+  //   if (!props.positive) {
+  //     return <p> No feedback given </p>;
+  //   }
+  return (
+    <div>
+      <table>
+        <tbody>
+          <Statistic text="Average" value={props.average} />
+          <Statistic text="Percent positive" value={props.positive} />
+          <Statistic text="Good" value={props.good} />
+          <Statistic text="Neutral" value={props.neutral} />
+          <Statistic text="Bad" value={props.bad} />
+          <Statistic text="Total" value={props.total} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-  const increaseOne = () => {
-    setCounter(counter + 1);
-    setAll(allClicks.concat("+1"));
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = good + bad + neutral;
+
+  const positive = (good / total) * 100;
+
+  const increaseGood = () => setGood(good + 1);
+
+  const increaseBad = () => setBad(bad + 1);
+
+  const increaseNeutral = () => setNeutral(neutral + 1);
+
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  ];
+
+  const voteArray = Array.apply(null, new Array(anecdotes.length)).map(
+    Number.prototype.valueOf,
+    0
+  );
+
+  const [selected, isSelected] = useState(0);
+  const [votes, setVotes] = useState(voteArray);
+  const randomizeAnecdote = (max) => {
+    isSelected(Math.floor(Math.random() * anecdotes.length));
+    console.log(selected);
   };
 
-  const setZero = () => {
-    setCounter(0);
-    setAll([]);
-    setClicks({
-      left: 0,
-      right: 0,
-    });
+  const copy = [...voteArray];
+
+  const handleVote = () => {
+    const newVoteArray = [...votes];
+    newVoteArray[selected] += 1;
+    console.log(newVoteArray);
+    setVotes(newVoteArray);
   };
 
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0,
-  });
-
-  const [allClicks, setAll] = useState([]);
-
-  const handleLeftClick = () => {
-    setClicks({ ...clicks, left: clicks.left + 1 });
-    setAll(allClicks.concat("L"));
-  };
-
-  const handleRightClick = () => {
-    setClicks({ ...clicks, right: clicks.right + 1 });
-    setAll(allClicks.concat("R"));
-  };
-
-  // setTimeout(() => setCounter(counter + 1), 1000);
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  const name = "Severus";
-  const age = 47;
+  const highestVoteIndex = [...votes].indexOf(Math.max(...votes));
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-      <Hello name={name} age={age} />
-      <p>It is now {now.toString()}</p>
-      <p>
-        {a} + {b} equals {a + b}
-      </p>
-
-      <Button handleClick={increaseOne} text="Add" />
-      <Button handleClick={setZero} text="Reset" />
-      <Display counter={counter} />
+      <div>
+        {" "}
+        <h2>FullStackOpen part1 exercises - unicafe</h2>
+        <Button handleClick={increaseGood} text="good" />
+        {good}
+        <Button handleClick={increaseNeutral} text="Neutral" />
+        {neutral}
+        <Button handleClick={increaseBad} text="Bad" />
+        {bad}
+        <Statistics
+          average={(total / 3).toFixed(2)}
+          positive={positive.toFixed(2)}
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+        />
+      </div>
 
       <div>
-        {clicks.left}
-        <Button handleClick={handleLeftClick} text="left" />
-        <Button handleClick={handleRightClick} text="right" />
-        {clicks.right}
+        <h2>part2 - anecdotes</h2>
+        <p>{anecdotes[selected]}</p>
+        <button onClick={handleVote}>Vote +1</button>
+        <button onClick={randomizeAnecdote}>Next anecdote</button>
+        <h2>max vote anecdote</h2>
+        <p>{Math.max(...votes)}</p>
+        {/* {[[...votes].indexOf(Math.max(...votes))]} */}
+        {anecdotes[highestVoteIndex]}
       </div>
-      <History allClicks={allClicks} />
     </div>
   );
 };
