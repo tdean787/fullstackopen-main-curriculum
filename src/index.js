0d0deccf76b4c75b8db5663aea25aac846c6e97d
Taylor <49215349+tdean787@users.noreply.github.com>
2021-04-21 20:27:14 -0500

    add json-server during development

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
+
+const promise = axios.get("http://localhost:3001/notes");
+console.log(promise);
 
 ReactDOM.render(<App />, document.getElementById("root"));
